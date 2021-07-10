64d46b7b8e51fde789c8b15d1edd7586dffa31f1
Karlo Iletić <kiletic@gmail.com>
2021-07-10 22:21:42 +0200

    Fix indentation and remove readme

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -5,10 +5,10 @@ const LoginForm = () => {
 		<form className = "LoginForm">
 			<h2 className = "logintext"> Login </h2>
 			<div className = "row"> 
-				<input type = "text" placeholder = "Username"/> <br/>
+				<input type = "text" placeholder = "Username"/>
 			</div>
 			<div className = "row"> 
-			<input type = "text" placeholder = "Password"/> <br/>
+				<input type = "text" placeholder = "Password"/>
 			</div>
 			<div className = "btn"> 
 				<input type = "submit"/>
