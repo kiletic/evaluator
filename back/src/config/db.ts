21a132443ada79a7db3b37a01bcc44a2b686f2ef
Karlo Iletić <kiletic@gmail.com>
2021-09-13 14:34:12 +0200

    Remove vulnerability O.o

--- back/src/config/db.ts
+++
@@ -1,4 +0,0 @@
-const dbURI: string = 'mongodb+srv://jeff:jeff@jeff.u9buz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const dbOPTS: any = { useNewUrlParser : true }; // hack
-
-export { dbURI, dbOPTS };
